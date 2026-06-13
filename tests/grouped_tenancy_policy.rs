3ed3741dec1fc01aa71ccacfb387e4c430522072
Filipe Forattini <filipe@forattini.com.br>
2026-06-13 19:28:51 -0300

    test(ci): group tenancy policy integration targets (#1172)

---
+++ tests/grouped_tenancy_policy.rs
@@ -0,0 +1,22 @@
+#![allow(dead_code)]
+
+#[path = "grouped/tenancy_policy/e2e_issue_745_red_typed_model_relations.rs"]
+mod e2e_issue_745_red_typed_model_relations;
+
+#[path = "grouped/tenancy_policy/e2e_issue_753_ddl_policy_aware.rs"]
+mod e2e_issue_753_ddl_policy_aware;
+
+#[path = "grouped/tenancy_policy/e2e_issue_755_queue_policy_aware.rs"]
+mod e2e_issue_755_queue_policy_aware;
+
+#[path = "grouped/tenancy_policy/e2e_issue_756_vector_policy_aware.rs"]
+mod e2e_issue_756_vector_policy_aware;
+
+#[path = "grouped/tenancy_policy/e2e_rls_universal.rs"]
+mod e2e_rls_universal;
+
+#[path = "grouped/tenancy_policy/e2e_tenancy_dotted.rs"]
+mod e2e_tenancy_dotted;
+
+#[path = "grouped/tenancy_policy/e2e_tenant_auto_index.rs"]
+mod e2e_tenant_auto_index;
