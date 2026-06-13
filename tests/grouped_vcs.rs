f5743e9215e3999444fa4afcdeea3c84cf9e66e2
Filipe Forattini <filipe@forattini.com.br>
2026-06-13 16:48:30 -0300

    test(ci): group VCS integration targets

---
+++ tests/grouped_vcs.rs
@@ -0,0 +1,16 @@
+#![allow(dead_code)]
+
+#[path = "grouped/vcs/e2e_vcs.rs"]
+mod e2e_vcs;
+
+#[path = "grouped/vcs/e2e_vcs_as_of_enforce.rs"]
+mod e2e_vcs_as_of_enforce;
+
+#[path = "grouped/vcs/e2e_vcs_as_of_parse.rs"]
+mod e2e_vcs_as_of_parse;
+
+#[path = "grouped/vcs/e2e_vcs_opt_in.rs"]
+mod e2e_vcs_opt_in;
+
+#[path = "grouped/vcs/e2e_vcs_phase5.rs"]
+mod e2e_vcs_phase5;
