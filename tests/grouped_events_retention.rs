920e856b0c7ac37bbe3ec20cc84fe9334b195ce7
Filipe Forattini <filipe@forattini.com.br>
2026-06-13 17:42:49 -0300

    test(ci): group events retention integration targets (#1163)

---
+++ tests/grouped_events_retention.rs
@@ -0,0 +1,13 @@
+#![allow(dead_code)]
+
+#[path = "grouped/events_retention/e2e_events_backfill.rs"]
+mod e2e_events_backfill;
+
+#[path = "grouped/events_retention/e2e_events_cdc_rid.rs"]
+mod e2e_events_cdc_rid;
+
+#[path = "grouped/events_retention/e2e_events_foundation.rs"]
+mod e2e_events_foundation;
+
+#[path = "grouped/events_retention/e2e_retention_sweeper.rs"]
+mod e2e_retention_sweeper;
