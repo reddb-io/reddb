fa9b1226f22137be41237b839d4c904c89591b3e
Filipe Forattini <filipe@forattini.com.br>
2026-06-13 18:43:50 -0300

    test(ci): group redwire protocol integration targets (#1167)

---
+++ tests/grouped_redwire_protocol.rs
@@ -0,0 +1,22 @@
+#![allow(dead_code)]
+
+#[path = "grouped/redwire_protocol/e2e_issue_762_redwire_output_stream.rs"]
+mod e2e_issue_762_redwire_output_stream;
+
+#[path = "grouped/redwire_protocol/e2e_issue_764_redwire_input_stream.rs"]
+mod e2e_issue_764_redwire_input_stream;
+
+#[path = "grouped/redwire_protocol/postgres_wire_extended.rs"]
+mod postgres_wire_extended;
+
+#[path = "grouped/redwire_protocol/redwire_oauth_e2e.rs"]
+mod redwire_oauth_e2e;
+
+#[path = "grouped/redwire_protocol/redwire_oauth_smoke.rs"]
+mod redwire_oauth_smoke;
+
+#[path = "grouped/redwire_protocol/redwire_queue_read_wait_smoke.rs"]
+mod redwire_queue_read_wait_smoke;
+
+#[path = "grouped/redwire_protocol/redwire_smoke.rs"]
+mod redwire_smoke;
