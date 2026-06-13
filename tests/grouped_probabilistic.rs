77fb2066455483832b835bd83b294e0b2e5e29ac
Filipe Forattini <filipe@forattini.com.br>
2026-06-13 18:59:27 -0300

    test(ci): group probabilistic integration targets

---
+++ tests/grouped_probabilistic.rs
@@ -0,0 +1,10 @@
+#![allow(dead_code)]
+
+#[path = "grouped/probabilistic/e2e_issue_542_probabilistic_commands.rs"]
+mod e2e_issue_542_probabilistic_commands;
+
+#[path = "grouped/probabilistic/e2e_issue_554_probabilistic_sql_read_forms.rs"]
+mod e2e_issue_554_probabilistic_sql_read_forms;
+
+#[path = "grouped/probabilistic/e2e_probabilistic_public_contract.rs"]
+mod e2e_probabilistic_public_contract;
