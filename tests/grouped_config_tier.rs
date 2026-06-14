8c36c2a82e95b8428f8e8a79c61c99223575c786
Filipe Forattini <filipe@forattini.com.br>
2026-06-13 23:15:29 -0300

    fix(cli): honor storage preset for dump restore (#1190)

--- tests/grouped_config_tier.rs
+++ tests/grouped_config_tier.rs
@@ -12,6 +12,9 @@ mod e2e_config_secret_ref;
 #[path = "grouped/config_tier/e2e_config_vault_observation.rs"]
 mod e2e_config_vault_observation;
 
+#[path = "grouped/config_tier/e2e_secret_sql.rs"]
+mod e2e_secret_sql;
+
 #[path = "grouped/config_tier/e2e_issue_480_tier_default_promotion_contract.rs"]
 mod e2e_issue_480_tier_default_promotion_contract;
 
