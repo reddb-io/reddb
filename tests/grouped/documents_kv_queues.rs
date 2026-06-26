c380fad64d62baeeb28bf0db3139d35019381571
Filipe Forattini <filipeforattini1@gmail.com>
2026-06-26 02:43:35 -0300

    test(document): register issue #1402 binary-body e2e in the doc group
    Refs #1402

--- tests/grouped/documents_kv_queues.rs
+++ tests/grouped/documents_kv_queues.rs
@@ -18,6 +18,9 @@ mod e2e_issue_535_red_queues_virtual_table;
 #[path = "docs_kv_queue/e2e_issue_540_documents_basics.rs"]
 mod e2e_issue_540_documents_basics;
 
+#[path = "docs_kv_queue/e2e_issue_1402_document_binary_body.rs"]
+mod e2e_issue_1402_document_binary_body;
+
 #[path = "docs_kv_queue/e2e_issue_541_kv_namespaced_keys.rs"]
 mod e2e_issue_541_kv_namespaced_keys;
 
