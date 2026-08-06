443a3256392321053d75913562d86f0f8419135a
filipeforattini <filipeforattini1@gmail.com>
2026-08-06 01:46:10 -0300

    refactor(server): import types keystone directly Refs #2164

--- crates/reddb-server/src/storage/query/planner/logical_helpers.rs
+++ crates/reddb-server/src/storage/query/planner/logical_helpers.rs
@@ -8,7 +8,7 @@ use crate::storage::query::ast::{
 use crate::storage::query::is_universal_entity_source as is_universal_query_source;
 use crate::storage::query::planner::stats_provider::CatalogStatsProvider;
 use crate::storage::query::sql_lowering::{effective_table_filter, effective_table_projections};
-use crate::storage::schema::Value;
+use reddb_types::Value;
 use crate::storage::RedDB;
 
 use super::{AccessPathDecision, CanonicalLogicalNode, CardinalityEstimate, CostEstimator};
